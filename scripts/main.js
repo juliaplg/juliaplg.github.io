d7a34d01cafa0728ce93db5df7e992cff1e3b3fc
Julia Prado <juliaplg@hotmail.com>
2025-05-07 04:23:55 -0300

    a

---
+++ scripts/main.js
@@ -0,0 +1,40 @@
+const myImage = document.querySelector("img");
+
+myImage.addEventListener("click", () => {
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/qiqi 3.png") {
+        myImage.setAttribute("src", "images/qiqi 2.png");
+    } else {
+        myImage.setAttribute("src", "images/qiqi 3.png");
+    }
+});
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Bem-vindo(a) ao meu fanclub, ${myName}`;
+}
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Bem-vindo(a) ao meu fanclub, ${storedName}`;
+}
+
+myButton.addEventListener("click", () => {
+    setUserName();
+});
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem("name", myName);
+        myHeading.textContent = `Bem-vindo(a) ao meu fanclub, ${myName}`;
+    }
+}
